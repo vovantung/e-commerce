77ff26a126d8d9d4eedcdf52511f525230a9ef99
vovantung <vovantungdt123@gmail.com>
2024-08-30 10:16:28 +0700

    cm10

--- shop/src/main/java/txu/shop/api/Api.java
+++ shop/src/main/java/txu/shop/api/Api.java
@@ -1,5 +1,6 @@
 package txu.shop.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping()
 public class Api {
+    @Value("${spring.datasource.url}")
+    private String url_db;
     @GetMapping("/health-check")
-    public void healthcheck(){
-        return;
+    public String healthcheck(){
+        return url_db ;
     }
 }
