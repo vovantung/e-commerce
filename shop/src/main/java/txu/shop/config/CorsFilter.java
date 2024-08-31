1c58a55f4c7ef4de426258101e0ebcacb6decc75
vovantung <vovantungdt123@gmail.com>
2024-08-31 16:37:30 +0700

    cm31

--- shop/src/main/java/txu/shop/config/CorsFilter.java
+++ shop/src/main/java/txu/shop/config/CorsFilter.java
@@ -6,14 +6,14 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Component
-public class CorsFilter implements Filter {
-
+//@Component
+public class CorsFilter{
+//public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Max-Age", "48000");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
         chain.doFilter(req, res);
     }
