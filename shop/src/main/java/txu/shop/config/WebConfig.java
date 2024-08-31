0f77724e90e50806d153d7751ed0c9c5fd5e9ef9
vovantung <vovantungdt123@gmail.com>
2024-08-31 17:25:06 +0700

    cm33

--- shop/src/main/java/txu/shop/config/WebConfig.java
+++ shop/src/main/java/txu/shop/config/WebConfig.java
@@ -5,12 +5,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+//@Configuration
+//@EnableWebMvc
+public class WebConfig {
+//public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**");
+//    }
 }
